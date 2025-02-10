e60adc83c69ddf019918639e5eadc54655a1684a
1Rudra <sk311h@gmail.com>
2025-02-10 17:21:02 +0530

    removed api

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-import openai
-# from PIL import Image
+from openai import OpenAI
+from PIL import Image
 import streamlit as st
 from config import apikey
 
-client = openai.Client(api_key=apikey)
+client = OpenAI(api_key=apikey) 
 
 def generate_image(image_discription, num_images):
-    img_response = openai.images.generate(
+    img_response=client.image.generate(
         model="dall-e-3",
         prompt=image_discription, 
         size="1024x1024",
@@ -14,7 +14,7 @@ def generate_image(image_discription, num_images):
         n=1
     )
     
-    image_url = img_response.data[0].url
+    image_url = img_response[0 .url]
     return image_url
 st.set_page_config(page_title="HoriZon", page_icon="camera", layout="wide")
 
@@ -25,6 +25,5 @@ img_discription = st.text_input("Enter the image discription")
 num_of_images = st.number_input("Number of images", min_value=1, max_value=10, value=1)
 
 if st.button("Generate image ") :
-    generate_image =generate_image(img_discription, num_of_images)
-    st.image (image_url, caption="generate_image") # type: ignore
-
+    generate_image=generate_image(img_discription, num_of_images)
+    st.image(generate_image)
